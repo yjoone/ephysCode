2a196fc0dee48d8d6b43bfd47ae1f57498e04b2c
Jim <yjoone@gmail.com>
2019-01-16 10:39:45 -0500

    Update savefigure.m

--- savefigure.m
+++ savefigure.m
@@ -2,7 +2,7 @@ function savefigure(fh,outfilepath,outfilename,varargin)
 % This function saves the figure on the figure handle (fh) in .fig and .png
 % format.
 
-assign(varargin)
+assign(varargin{:})
 
 savefig(fh,fullfile(outfilepath,[outfilename '.fig']))
 print(fh,fullfile(outfilepath,[outfilename '.png']),'-dpng')
