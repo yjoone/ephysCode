b1e3a49088f518e441df314c9574cbdbdb5182b3
Jim <yjoone@gmail.com>
2019-05-29 14:02:12 -0400

    created Test
    test file for Hong

---
+++ Test.m
@@ -0,0 +1,5 @@
+% example script for ephys analysis
+
+filename = 'R:\LiuLab\People\Jim\Experiments\OTmanipEphysExpt\Experiments_NL\HPractice\HAbituation1\Habituation2_HPractice_051719.hex';
+
+[datacell,samplerate] = readNL_gka(filename);
